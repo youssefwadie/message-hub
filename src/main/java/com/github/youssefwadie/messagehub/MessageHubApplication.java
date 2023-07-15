68e8023d3c7670b8ebd8d7b4f500b9048844d83d
Youssef Wadie <youssefwadie14@gmail.com>
2023-07-15 20:45:31 +0300

    datastax hosted cassandra configuration

--- src/main/java/com/github/youssefwadie/messagehub/MessageHubApplication.java
+++ src/main/java/com/github/youssefwadie/messagehub/MessageHubApplication.java
@@ -1,10 +1,15 @@
 package com.github.youssefwadie.messagehub;
 
+import com.github.youssefwadie.messagehub.config.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
+@EnableConfigurationProperties({
+		DataStaxAstraProperties.class
+})
 public class MessageHubApplication {
 
 	public static void main(String[] args) {
